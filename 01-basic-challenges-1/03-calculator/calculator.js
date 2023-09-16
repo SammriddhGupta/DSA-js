d46a012ed73dc2420254d910c12a9b97770d1542
SammriddhGupta <sammriddhgupta@gmail.com>
2023-09-17 00:08:02 +1000

    basic1 progress

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,25 @@
-function calculator() {}
+function calculator(num1,num2,op) {
+    let result;
+
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            throw new Error('Invalid Operator');
+    }
+
+    return result;
+
+}
 
 module.exports = calculator;
--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,15 @@
-function countOccurrences() {}
+function countOccurrences(word, find) {
+    let count = 0;
+    
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] == find)
+            count++;
+    }
+    return count
+
+}
+
+// 1 liner solution: 
+/* const countOccurrences = (word, find) => word.split(find).length - 1; */
 
 module.exports = countOccurrences;
