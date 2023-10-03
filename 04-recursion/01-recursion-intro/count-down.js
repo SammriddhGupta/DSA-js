97a42b19b5b548c70ff7211fce6e6d864c77f54a
SammriddhGupta <sammriddhgupta@gmail.com>
2023-10-03 23:56:29 +1100

    finished arrays

--- 04-recursion/01-recursion-intro/count-down.js
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -1,3 +1,17 @@
-function countDown() {}
+// print all numbers from num to 0
+function countDown(num) {
+
+    // base case 
+    if (num <= 0) {
+        console.log("All Done!");
+        return;
+
+    }
+
+    // recursive case
+    console.log(num);
+    num--;
+    countDown(num);
+}
 
 module.exports = countDown;
