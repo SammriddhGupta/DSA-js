a946dfb31127778c8148e6f7cea2a59d951cca1d
SammriddhGupta <sammriddhgupta@gmail.com>
2023-10-09 00:32:55 +1100

    time complexity done

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,3 +1,17 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+    let maxSum = -Infinity;
+
+    for (let i = 0; i <= arr.length - k; i++) {
+        let currSum = 0;
+        
+        for (let j = i; j < i+k; j++) {
+            currSum += arr[j];
+        }
+
+        maxSum = Math.max(maxSum, currSum);
+    }
+
+    return maxSum;
+}
 
 module.exports = maxSubarraySum;
